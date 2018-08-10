75d019c20c5472c4fd913e8d995a18579b8c5924
Lorival Souza <lorivalnew@gmail.com>
2018-08-09 21:34:10 -0300

    Ajuste no endpoint /categoria/{id} criação de exception personalizada

--- src/main/java/com/lorival/cursomc/services/CategoriaService.java
+++ src/main/java/com/lorival/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.lorival.cursomc.domain.Categoria;
 import com.lorival.cursomc.repositories.CategoriaRepository;
+import com.lorival.cursomc.sevices.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -13,7 +14,10 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	
 	public Categoria buscar(Integer id) {		
-		Categoria obj = repo.findOne(id);		
+		Categoria obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo : " + Categoria.class.getName());
+		}
 		return obj;
 	}
 
