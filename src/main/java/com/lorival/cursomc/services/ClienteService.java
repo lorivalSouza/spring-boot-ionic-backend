f07713e681d16d186cb6bbe7545f61bbcd9471b6
Lorival Souza <lorivalnew@gmail.com>
2018-08-13 14:33:08 -0300

    Recursos PUT, DELETE e UPDATE

--- src/main/java/com/lorival/cursomc/services/ClienteService.java
+++ src/main/java/com/lorival/cursomc/services/ClienteService.java
@@ -1,10 +1,18 @@
 package com.lorival.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.lorival.cursomc.domain.Cliente;
+import com.lorival.cursomc.dto.ClienteDTO;
 import com.lorival.cursomc.repositories.ClienteRepository;
+import com.lorival.cursomc.sevices.exceptions.DataIntegrityException;
 import com.lorival.cursomc.sevices.exceptions.ObjectNotFoundException;
 
 @Service
@@ -19,6 +27,45 @@ public class ClienteService {
 			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo : " + Cliente.class.getName());
 		}
 		return obj;
+	}
+	
+	public Cliente update(Cliente obj) {
+		//Verifica se Id existe
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir, pois há entidades relacionadas.");
+		}
+		
+	}
+	
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromClienteDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+		
+	}
+	
+	//metodo auxiliar so vai ser usado dentro da classe por isso private
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+		
 	}
 
 }
